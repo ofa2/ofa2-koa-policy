ac01aa308cb641efb41fb3f2f197bda2a6bc12b8
xinshangshangxin <xinshangshangxin@gmail.com>
2018-05-11 09:42:26 +0800

    chore: remove build uglify

--- dist/bundle.cjs.js
+++ dist/bundle.cjs.js
@@ -1,2 +1,63 @@
-"use strict";function _interopDefault(e){return e&&"object"==typeof e&&"default"in e?e.default:e}var _=_interopDefault(require("lodash")),path=_interopDefault(require("path")),filePathOneLayer=_interopDefault(require("file-path-one-layer"));function lift(){let e=this.policies={},i=this.config.paths.policies=path.join(this.projectPath,"policies");return filePathOneLayer(i).then(i=>{i.forEach(i=>{let t=require(i.path);e[i.name]=t.default?t.default:t})}).then(()=>{this.config.controllerActionPolicies={},this.config.policies=this.config.policies||{};let e=this.config.policies["*"]||[];_.forEach(this.controllers,(i,t)=>{let o=this.config.policies[t]||{},l=o["*"]||e;_.forEach(i,(e,i)=>{_.isFunction(e)&&(this.config.controllerActionPolicies[`${t}.${i}`]=o[i]||l)})});let i=[];return this.controllerActionPolicies=_.mapValues(this.config.controllerActionPolicies,e=>_.map(e,e=>{let t=this.policies[e];return t||i.push(e),t})),i.length?Promise.reject(new Error(`Unknown policy:${i.join(",")}`)):Promise.resolve()})}module.exports=lift;
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var _ = _interopDefault(require('lodash'));
+var path = _interopDefault(require('path'));
+var filePathOneLayer = _interopDefault(require('file-path-one-layer'));
+
+function lift() {
+  /* eslint-disable global-require */
+
+  /* eslint-disable import/no-dynamic-require */
+
+  /* eslint max-len: ["error", 150] */
+  // eslint-disable-next-line no-multi-assign
+  let policies = this.policies = {}; // eslint-disable-next-line no-multi-assign
+
+  let policiesPath = this.config.paths.policies = path.join(this.projectPath, 'policies');
+  return filePathOneLayer(policiesPath).then(files => {
+    files.forEach(file => {
+      let config = require(file.path);
+
+      policies[file.name] = config.default ? config.default : config;
+    });
+  }).then(() => {
+    this.config.controllerActionPolicies = {};
+    this.config.policies = this.config.policies || {};
+    let defaultPolicies = this.config.policies['*'] || [];
+
+    _.forEach(this.controllers, (controller, controllerName) => {
+      let controllerPolicies = this.config.policies[controllerName] || {};
+      let defaultControllerPolicies = controllerPolicies['*'] || defaultPolicies;
+
+      _.forEach(controller, (action, actionName) => {
+        if (_.isFunction(action)) {
+          this.config.controllerActionPolicies[`${controllerName}.${actionName}`] = controllerPolicies[actionName] || defaultControllerPolicies;
+        }
+      });
+    });
+
+    let unknownPolicies = [];
+    this.controllerActionPolicies = _.mapValues(this.config.controllerActionPolicies, policyNames => {
+      return _.map(policyNames, policyName => {
+        let policy = this.policies[policyName];
+
+        if (!policy) {
+          unknownPolicies.push(policyName);
+        }
+
+        return policy;
+      });
+    });
+
+    if (unknownPolicies.length) {
+      return Promise.reject(new Error(`Unknown policy:${unknownPolicies.join(',')}`));
+    }
+
+    return Promise.resolve();
+  });
+}
+
+module.exports = lift;
 //# sourceMappingURL=bundle.cjs.js.map
